d74f39684593d56fde1922629e01de10079f1b0a
Ed Asriyan <ed-asriyan@protonmail.com>
2017-10-10 00:51:40 +0300

    Create subproject

---
+++ server/Server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ed Asriyan on 30/09/2017.
+//
+
+#include "Server.hpp"
---
+++ server/Server.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Ed Asriyan on 30/09/2017.
+//
+
+#ifndef STATIC_SERVER_SERVER_HPP
+#define STATIC_SERVER_SERVER_HPP
+
+namespace server {
+	class Server {
+
+	};
+}
+
+#endif //STATIC_SERVER_SERVER_HPP
