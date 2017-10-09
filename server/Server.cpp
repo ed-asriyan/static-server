93d782fdd24f85dbbaedce91b75472f613500e02
Ed Asriyan <ed-asriyan@protonmail.com>
2017-10-10 01:14:03 +0300

    Create echo server

--- server/Server.cpp
+++ server/Server.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "Server.hpp"
+#include <boost/asio/yield.hpp>
+
+server::Server::Server(boost::asio::io_service& io_service, const std::string& address, const std::string& port) {
+	tcp::resolver resolver(io_service);
+	tcp::resolver::query query(address, port);
+	acceptor.reset(new tcp::acceptor(io_service, *resolver.resolve(query)));
+}
+
+void server::Server::operator()(boost::system::error_code ec, size_t length) {
+	if (ec) {
+		return;
+	}
+
+	reenter (this) {
+		do {
+			socket.reset(new tcp::socket(acceptor->get_io_service()));
+			yield acceptor->async_accept(*socket, *this);
+			fork Server(*this)();
+		} while (is_parent());
+
+		buffer.reset(new boost::array<char, Server::BUFFER_SIZE>);
+
+		do {
+			yield socket->async_read_some(boost::asio::buffer(*buffer), *this);
+			yield socket->async_write_some(boost::asio::buffer(*buffer), *this);
+		} while (buffer.operator*()[0] != '.');
+
+		yield boost::asio::async_write(*socket, boost::asio::buffer("end\n", 5), *this);
+		socket->shutdown(tcp::socket::shutdown_both, ec);
+	}
+}
+
--- server/Server.hpp
+++ server/Server.hpp
@@ -5,9 +5,35 @@
 #ifndef STATIC_SERVER_SERVER_HPP
 #define STATIC_SERVER_SERVER_HPP
 
-namespace server {
-	class Server {
+#include <string>
 
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+
+namespace server {
+	class Server : boost::asio::coroutine {
+	public:
+		explicit Server(
+			boost::asio::io_service& io_service,
+			const std::string& address,
+			const std::string& port
+		);
+
+		void operator()(
+			boost::system::error_code ec = boost::system::error_code(),
+			std::size_t length = 0
+		);
+
+	private:
+		static constexpr unsigned int BUFFER_SIZE = 8192;
+
+		typedef boost::asio::ip::tcp tcp;
+
+		boost::shared_ptr<tcp::acceptor> acceptor;
+		boost::shared_ptr<tcp::socket> socket;
+		boost::shared_ptr<boost::array<char, Server::BUFFER_SIZE>> buffer;
 	};
 }
 
