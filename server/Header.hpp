218c7ab2cd85f5caced5a6b4ed9fdc5a503979f5
Ed Asriyan <ed-asriyan@protonmail.com>
2017-10-10 14:03:31 +0300

    Create struct Header

---
+++ server/Header.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Ed Asriyan on 10/10/2017.
+//
+
+#ifndef STATIC_SERVER_HEADER_H
+#define STATIC_SERVER_HEADER_H
+
+#include <string>
+
+namespace server {
+	struct Header {
+		std::string name;
+		std::string value;
+	};
+}
+
+#endif //STATIC_SERVER_HEADER_H
